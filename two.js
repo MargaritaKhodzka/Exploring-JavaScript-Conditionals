af45bd0d716a71859c369f44aaefbc4d440925c9
MargaritaKhodzka <mkhodzka@gmail.com>
2019-07-10 17:03:49 -0400

    ternary operator

--- two.js
+++ two.js
@@ -1,7 +1,10 @@
-var isTrue = false;
+let isTrue = false;
 
-if (isTrue) {
-	console.log('yes');
-} else {
-	console.log('no');
-}
+let yesOrNo = isTrue ? 'yes' : 'no';
+console.log(yesOrNo);
+
+// if (isTrue) {
+// 	console.log('yes');
+// } else {
+// 	console.log('no');
+// }
